2c8b1c779d98821c2614f291456e20d9e71d0dc8
lime-in-the-coconut <limesinthecoconut@gmail.com>
2013-12-04 10:40:27 -0500

    added email order detail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Thundercats::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { host: "thundercat-jenny.herokuapp.com"}
 end
